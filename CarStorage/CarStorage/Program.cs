ba498d34f21698faab1236aaed116e8781fb9ba1
Naka0405 <naka04054840@gmail.com>
2020-03-19 20:20:14 +0300

    changed

--- CarStorage/CarStorage/Program.cs
+++ CarStorage/CarStorage/Program.cs
@@ -6,7 +6,21 @@ namespace CarStorage
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car[] cars = new Car[3];
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i] = new Car();
+                Console.WriteLine("Enter cars name:");
+                cars[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Enter cars price:");
+                cars[i].Price = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter color of car:");
+                cars[i].Color = Console.ReadLine();
+            }
+            Console.WriteLine($"Sourse price: {cars[0].Price}; Price after  sale: {cars[0].GetSale()}");
+
         }
     }
 }
